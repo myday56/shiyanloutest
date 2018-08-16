d13d4a9eeac608383790953000d187f6fca5652e
myday56 <leletou56@live.com>
2018-08-16 11:33:21 +0800

    tiaozhan 7

---
+++ 10/getrank.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+import sys
+from pymongo import MongoClient
+
+def get_rank(user_id):
+    client = MongoClient()
+    db = client.shiyanlou
+    contests = db.contests
+    
+    TODO
+
+
+    return rank,score,submit_time
+
+if __name__ == '__main__':
+    
+    TODO
+
+    userdata = get_rank(user_id)
+    print(userdata)
