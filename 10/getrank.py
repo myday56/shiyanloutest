56e5c646febd41d878fd941b7dd8b179dabf5932
myday56 <leletou56@live.com>
2018-08-17 01:53:29 +0800

    tiaozhan 10

--- 10/getrank.py
+++ 10/getrank.py
@@ -1,5 +1,4 @@
 # -*- coding:utf-8 -*-
-
 import sys
 from pymongo import MongoClient
 
@@ -7,14 +6,37 @@ def get_rank(user_id):
     client = MongoClient()
     db = client.shiyanlou
     contests = db.contests
+    contest_order_by_score = contests.aggregate(
+            [
+            {'$group':
+                {'_id':'$user_id',
+                'total_score':{'$sum':'$score'},
+                'total_time':{'$sum':'$submit_time'}
+                }
+            },
+            {'$sort':
+                {'total_score':-1},
+            }
+            ])
+    rank_start = 1
+    user_rank_rs = {}
+    user_rank_details = []
+    for i in contest_order_by_score:
+        user_rank_details =[rank_start,i['total_score'],i['total_time']]
+        rank_start += 1
+        user_rank_rs[i['_id']] = user_rank_details
     
-    
-
+    # print(len(user_rank_rs))
+    rank = user_rank_rs[user_id][0]
+    score = user_rank_rs[user_id][1]
+    submit_time = user_rank_rs[user_id][2]
     return rank,score,submit_time
 
 if __name__ == '__main__':
-    
-    TODO
-
-    userdata = get_rank(user_id)
-    print(userdata)
+    try:
+        user_id = int(sys.argv[1])
+    except ValueError:
+        print("Parameter Error")
+    else:
+        userdata = get_rank(user_id)
+        print(userdata)
--- 10/test.py
+++ 10/test.py
@@ -1,10 +1,35 @@
-import pymongo
+import sys
 import pprint
 from pymongo import MongoClient
 client = MongoClient()
 db = client.shiyanlou
 contests = db.contests
-pprint.pprint(contests.find_one({'user_id':3}))
-contest_order_by_score = contests.aggregate([{'$group':{'use_id':'$user_id','total_score':{'$sum':'$score'},'total_time':{'$sum':'$submit_time'}}}])
-for x in contest_order_by_score:
-    print(x)
+contest_order_by_score = contests.aggregate(
+        [
+        {'$group':
+            {'_id':'$user_id',
+            'total_score':{'$sum':'$score'},
+            'total_time':{'$sum':'$submit_time'}
+            }
+        },
+        {'$sort':
+            {'total_score':-1},
+        }
+        ])
+rank_start = 1
+user_rank_rs = {}
+user_rank_details = []
+for i in contest_order_by_score:
+    user_rank_details =[rank_start,i['total_score'],i['total_time']]
+    rank_start += 1
+    user_rank_rs[i['_id']] = user_rank_details
+# print(user_rank_rs[2][0])
+rank = user_rank_rs[2][0]
+score = user_rank_rs[2][1]
+submit_time = user_rank_rs[2][2]
+print(rank,score,submit_time)
+
+
+
+
+     
