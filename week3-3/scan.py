38e7844ccfbd32095a80b3a4f51894529fd50513
myday56 <leletou56@live.com>
2018-08-21 22:45:06 +0800

    week3-3

--- week3-3/scan.py
+++ week3-3/scan.py
@@ -4,11 +4,9 @@ host_ip = opts[0][1]
 host_port = opts[1][1]
 
 def connect_socket(host,port):
-    host = host_ip
-    port = host_port
     s = socket.socket()
     s.settimeout(0.1)
-    if int(s.connect_ex((host,port))) == 0:
+    if s.connect_ex((host,port)) == 0:
         print(port,'open')
     else:
         print(port,'closed')
@@ -16,12 +14,10 @@ def connect_socket(host,port):
 if len(host_ip.split('.')) == 4:
    if len(args) == 0:
         if '-' not in host_port:
-        	connect_socket(host_ip,host_port)
-            # print(host_port)
+        	connect_socket(host_ip,int(host_port))
         else:
             a = host_port.split('-')
-            # print(a[0],a[1])
             for i in range(int(a[0]),int(a[1])+1):
-                connect_socket(host_ip,host_port)
+                connect_socket(host_ip,i)
 else:
-    print('sth wrong')
+    print('Parameter Error')
