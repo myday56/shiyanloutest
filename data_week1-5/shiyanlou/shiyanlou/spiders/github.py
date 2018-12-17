b8052a4c708cc18f1f3c6db7ce1958bfbea92468
myday56 <leletou56@live.com>
2018-12-17 17:11:39 +0800

    week1-5

--- data_week1-5/shiyanlou/shiyanlou/spiders/github.py
+++ data_week1-5/shiyanlou/shiyanlou/spiders/github.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from shiyanlou.items import ShiyanlouItem
 
 
 class GithubSpider(scrapy.Spider):
     name = 'github'
     allowed_domains = ['github.com']
-    start_urls = ['http://github.com/shiyanlou?tab=repositories']
+    start_urls = ['https://github.com/shiyanlou?tab=repositories']
+    # @property 
+    # def starts_urls(self):
+    #     url_tmpl = 'https://github.com/shiyanlou?tab=repositories'
+        
 
     def parse(self, response):
         repos = response.xpath('//div[@class="col-9 d-inline-block"]')
         for repo in repos:
-            item = ShiyanlouItem()
-            item['repo_name'] = repo.xpath('.//div[@clacc="d-inline-block mb-1"]/text()').extract_first()
-            item['update_time'] = repo.xpath('.//div[@class=""]/text()').extract_first()
+            item = ShiyanlouItem({
+                'repo_name' : repo.xpath('.//h3/a/text()').re_first('\\n\\s*(.+)'),
+                'update_time' : repo.xpath('.//relative-time').re_first('<relative-time datetime="(.{20})')
+            })
             yield item
+        for url in response.xpath('//div[@class="pagination"]/a[contains(text(),"Next")]'):
+            yield response.follow(url,callback=self.parse)
