79e892e63119300b589d5bd0deff4937af214de0
myday56 <leletou56@live.com>
2018-12-20 01:32:20 +0800

    week2-1

---
+++ data_week2-1/titanic.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+def plot():
+    df = sns.load_dataset("titanic")
+    fig,axes= plt.subplots(ncols=3, nrows=1, figsize=(12, 5))
+    sns.distplot(df['age'].dropna(),ax=axes[0])
+    sns.countplot(x="sex", hue="alive", data=df,ax=axes[1])
+    sns.countplot(x="class", hue="alive", data=df,ax=axes[2])
+    return axes
+
+plot()
